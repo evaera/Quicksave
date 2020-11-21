ecddc21d45705543803ae176e3d0472efa058439
Micah Hinckley <hinckleytmicah@gmail.com>
2020-11-20 16:52:23 -0800

    Add DataStoreLayer

---
+++ src/Layers/DataStoreLayer.lua
@@ -0,0 +1,21 @@
+local DataStoreService = game:GetService("DataStoreService")
+
+local DataStoreLayer = {
+    _dataStores = {};
+}
+
+function DataStoreLayer._getDataStore(collectionName)
+    if DataStoreLayer._dataStores[collectionName] == nil then
+        DataStoreLayer._dataStores[collectionName] = DataStoreService:GetDataStore(collectionName, "_package/eryn.io/quicksave")
+    end
+
+    return DataStoreLayer._dataStores[collectionName]
+end
+
+function DataStoreLayer.perform(methodName, collectionName, ...)
+    local dataStore = DataStoreLayer._getDataStore(collectionName)
+
+    return dataStore[methodName](dataStore, ...)
+end
+
+return DataStoreLayer
