9fff29630c763956b8350e9eb05c0dbe92e9a736
Micah Hinckley <hinckleytmicah@gmail.com>
2020-11-20 22:34:06 -0800

    Add Collection tests

--- src/Collection.spec.lua
+++ src/Collection.spec.lua
@@ -17,13 +17,55 @@ return function()
             defaultData = {};
         })
     end)
+    
+    describe("getDocument", function()
+        it("should be able to get documents", function()
+            local document = collection:getDocument("foobar"):expect()
+    
+            expect(document.collection).to.equal(collection)
+            expect(document.name).to.equal("foobar")
+    
+            expect(collection:getDocument("foobar"):expect()).to.equal(document)
+        end)
 
-    it("should be able to get documents", function()
-        local document = collection:getDocument("foobar"):expect()
+        -- TODO: Test getting just closed documents. It isn't very possible because it yields.
+    end)
 
-        expect(document.collection).to.equal(collection)
-        expect(document.name).to.equal("foobar")
+    describe("keyExists", function()
+        it("should return true when the key exists", function()
+            expect(collection:keyExists("foo")).to.equal(true)
+        end)
 
-        expect(collection:getDocument("foobar"):expect()).to.equal(document)
+        it("should return false when the key doesn't exist", function()
+            expect(collection:keyExists("bar")).to.equal(false)
+        end)
+    end)
+
+    describe("validateData", function()
+        it("should validate correct data", function()
+            expect(collection:validateData({
+                ["1234"] = "foo";
+            })).to.equal(true)
+        end)
+
+        it("should invalidate incorrect data", function()
+            expect(collection:validateData({
+                ["1234"] = "bar";
+            })).to.equal(false)
+        end)
+    end)
+
+    describe("validateKey", function()
+        it("should return true when key validates", function()
+            expect(collection:validateKey("1234", "foo")).to.equal(true)
+        end)
+
+        it("should return false when key invalidates", function()
+            expect(collection:validateKey("1234", "bar")).to.equal(false)
+        end)
+
+        it("should return false when key doesn't exist", function()
+            expect(collection:validateKey("bar", "baz")).to.equal(false)
+        end)
     end)
 end
